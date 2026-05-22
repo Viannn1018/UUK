4094feb44f35bb13b4a4b3e8e2f4a8b1b01ae0dc
Viannn1018 <putraferry752@gmail.com>
2026-05-22 22:40:04 +0700

    Delete assets directory

--- assets/script/script.js
+++
@@ -1,8 +0,0 @@
-window.onscroll = function() {
-            var nav = document.getElementById('mainNav');
-            if (window.pageYOffset > 50) {
-                nav.classList.add("navbar-scrolled");
-            } else {
-                nav.classList.remove("navbar-scrolled");
-            }
-        };
